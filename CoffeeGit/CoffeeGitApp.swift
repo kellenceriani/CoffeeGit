b315277013c9e618902db8c954012ccfa46a94bb
kellenceriani <kmceriani@cox.net>
2023-04-28 10:57:33 -0600

    Initial Commit

---
+++ CoffeeGit/CoffeeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeGitApp.swift
+//  CoffeeGit
+//
+//  Created by rick zanotti on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
